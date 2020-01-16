099e75af7f87622d598d13a7c2b6375b54c87978
andybeet <22455149+andybeet@users.noreply.github.com>
2020-01-16 17:00:17 -0500

    made data pull from databases its own standalone function

---
+++ R/get_comland_data.R
@@ -0,0 +1,90 @@
+#' Extracts commercial data from Database
+#'
+#' Connects to cfdbs and pulls fields from WOLANDS, WODETS, CFDETS
+#'
+#'@param channel an Object inherited from \link[DBI]{DBIConnection-class}. This object is used to connect
+#' to communicate with the database engine. (see \code{\link{connect_to_database}})
+#'@param endyear Numeric Scalar. Final year of query.
+#'@param landed Character String. Use landed weight for scallops and clams instead of live weight.
+#'@param out.dir path to directory where final output will be saved
+#'
+#'
+#'@return Data frame (data.table) (n x 10)
+#'Each row of the data.table represents a species record for a given tow/trip
+#'
+#'\item{YEAR}{Year of trip/tow}
+#'\item{MONTH}{Month of trip/tow}
+#'\item{NEGEAR}{Fishing gear used on trip/tow}
+#'\item{TONCL1}{Tonnage class of the fishing vessel}
+#'\item{NESPP3}{Species code (3 charachters)}
+#'\item{NESPP4}{Species code and market code (4 characters)}
+#'\item{AREA}{Statistical area in which species was reportly caught}
+#'\item{UTILCD}{Utilization code}
+#'\item{SPPLIVLB}{live weight (landed = "n") or landed weight (landed="y") in lbs}
+#'\item{SPPVALUE}{The value of landed catch to the nearest dollar (U.S.), paid to fisherman by dealer, for a given species.}
+#'
+#'@section File Creation:
+#'
+#'A file containing the data.table above will also be saved to the users machine in the directory provided
+#'
+#'@export
+
+get_comland_data <- function(channel,landed,endyear,out.dir) {
+
+
+  message("Pulling landings data from database. This could take a while (15 minutes) ... ")
+  #Landings
+  tables <- c(paste0('WOLANDS', 64:81),
+              paste0('WODETS',  82:93),
+              paste0('CFDETS',  1994:endyear, 'AA'))
+
+  #Generate one table
+  comland <- c()
+  for(i in 1:length(tables)){
+    landings.qry <- paste("select year, month, negear, toncl1, nespp3, nespp4, area,
+                           spplivlb, spplndlb, sppvalue, utilcd
+                           from", tables[i])
+
+    comland.yr <- as.data.table(DBI::dbGetQuery(channel, landings.qry))
+
+    data.table::setkey(comland.yr,
+                       YEAR,
+                       MONTH,
+                       NEGEAR,
+                       TONCL1,
+                       NESPP3,
+                       NESPP4,
+                       AREA,
+                       UTILCD)
+
+    message("Pulled data from ",tables[i]," ...")
+
+    # Use landed weight instead of live weight for shellfish
+    if(landed == 'y') {comland.yr[NESPP3 %in% 743:800, SPPLIVLB := SPPLNDLB]}
+
+    #Sum landings and value
+    #landings
+    comland.yr[, V1 := sum(SPPLIVLB), by = key(comland.yr)]
+    #value
+    #Fix null values
+    comland.yr[is.na(SPPVALUE), SPPVALUE := 0]
+    comland.yr[, V2 := sum(SPPVALUE), by = key(comland.yr)]
+
+    #Remove extra rows/columns
+    comland.yr <- unique(comland.yr, by = key(comland.yr))
+    comland.yr[, c('SPPLIVLB', 'SPPLNDLB', 'SPPVALUE') := NULL]
+
+    #Rename summed columns
+    data.table::setnames(comland.yr, c('V1', 'V2'), c('SPPLIVLB', 'SPPVALUE'))
+
+    comland <- data.table::rbindlist(list(comland, comland.yr))
+  }
+
+  # Save file.
+  if(landed == 'n') save(comland, file = file.path(out.dir, paste0("comland_raw_US_",Sys.Date(),".RData")))
+  if(landed == 'y') save(comland, file = file.path(out.dir, paste0("comland_raw_US_meatwt_",Sys.Date(),".RData")))
+
+  return(comland)
+
+}
+
