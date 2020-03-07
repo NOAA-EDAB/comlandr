c9ddb4467d47dfb621d6fd281d7a252816919df8
andybeet <22455149+andybeet@users.noreply.github.com>
2020-03-07 17:38:03 -0500

    aggregation step moved into its own function

--- R/Comland.r
+++ R/Comland.r
@@ -226,52 +226,14 @@ comland <- comland_menhaden(comland)
 #save(comland, file = paste(out.dir, "Comland_unkA.RData", sep = ''))
 
 
-#Deal with unknowns-------------------------------------------------------------------------
+#Deal with unknowns  -------------------------------------
+#1 - drop unknown species/landings
 comland <- comland_unknowns(comland)
 
 
 #2 - aggregate by quarter year, half year, major gear, and small/large TC
-comland[MONTH %in% 1:3,   QY := 1]
-comland[MONTH %in% 4:6,   QY := 2]
-comland[MONTH %in% 7:9,   QY := 3]
-comland[MONTH %in% 10:12, QY := 4]
-comland[MONTH == 0,       QY := 0]
+comland.agg <- comland_aggregate(comland,GEARS)
 
-comland[MONTH %in% 1:6,  HY := 1]
-comland[MONTH %in% 7:12, HY := 2]
-comland[MONTH == 0,      HY := 0]
-
-
-comland[NEGEAR %in% GEARS$otter,     GEAR := 'otter']
-comland[NEGEAR %in% GEARS$dredge.sc, GEAR := 'dredge.sc']
-comland[NEGEAR %in% GEARS$pot,       GEAR := 'pot']
-comland[NEGEAR %in% GEARS$longline,  GEAR := 'longline']
-comland[NEGEAR %in% GEARS$seine,     GEAR := 'seine']
-comland[NEGEAR %in% GEARS$gillnet,   GEAR := 'gillnet']
-comland[NEGEAR %in% GEARS$midwater,  GEAR := 'midwater']
-comland[NEGEAR %in% GEARS$dredge.o,  GEAR := 'dredge.o']
-comland[NEGEAR == 0,           GEAR := 'unknown']
-comland[is.na(GEAR),           GEAR := 'other']
-comland[, GEAR := as.factor(GEAR)]
-
-comland[TONCL1 %in% 1:3, SIZE := 'small']
-comland[TONCL1 > 3,      SIZE := 'large']
-comland[TONCL1 == 0,     SIZE := 'unknown']
-comland[, SIZE := as.factor(SIZE)]
-
-data.table::setkey(comland,
-       YEAR,
-       QY,
-       HY,
-       GEAR,
-       SIZE,
-       AREA,
-       NESPP3,
-       UTILCD)
-
-comland.agg <- comland[, list(sum(SPPLIVMT), sum(SPPVALUE)), by = key(comland)]
-
-data.table::setnames(comland.agg, c('V1', 'V2'), c('SPPLIVMT', 'SPPVALUE'))
 
 #3 - Use proportions of known catch to assign unknown catch
 #3.A QY/HY------------------------------------------------------------------------------
