5ad0cf2f4a893cc4b0527c3d04ec96b59d363ea0
andybeet <22455149+andybeet@users.noreply.github.com>
2020-03-25 14:56:36 -0400

    updated comland_herring to convert data types to RODBC equivalent

--- R/comland_herring.R
+++ R/comland_herring.R
@@ -7,6 +7,8 @@
 #'
 #'@return Processed Herring data added to comland
 #'
+#'@importFrom data.table ":=" "key"
+#'
 #'
 
 comland_herring <- function(channel,comland) {
@@ -14,30 +16,49 @@ comland_herring <- function(channel,comland) {
   herr.qry <- "select year, month, stock_area, negear, gearname, keptmt, discmt
              from maine_herring_catch"
 
+  #herr.catch <- as.data.table(RODBC::sqlQuery(channel, herr.qry))
   herr.catch <- data.table::as.data.table(DBI::dbGetQuery(channel, herr.qry))
+
+  herr.catch$YEAR <- as.integer(herr.catch$YEAR)
+  herr.catch$MONTH <- as.integer(herr.catch$MONTH)
+  herr.catch$STOCK_AREA <- as.integer(herr.catch$STOCK_AREA)
+  herr.catch$NEGEAR <- as.integer(herr.catch$NEGEAR)
+  herr.catch$GEARNAME <- as.factor(herr.catch$GEARNAME)
+
+
   data.table::setkey(herr.catch, YEAR, MONTH, STOCK_AREA, NEGEAR)
 
-  herring <- herr.catch[, list(sum(KEPTMT), sum(DISCMT)), by = eval(data.table::key(herr.catch))]
+
+
+  herring <- herr.catch[, list(sum(KEPTMT), sum(DISCMT)), by = key(herr.catch)]
+
+
   data.table::setnames(herring, c('STOCK_AREA', 'V1', 'V2'),
                        c('AREA', 'SPPLIVMT', 'DISCMT'))
 
-  herring$YEAR <- as.integer(herring$YEAR)
-  herring$MONTH <- as.integer(herring$MONTH)
-#  herring$STOCK_AREA <- as.integer(herring$STOCK_AREA)
-  herring$NEGEAR <- as.integer(herring$NEGEAR)
-  herring$MONTH <- as.integer(herring$MONTH)
+
 
   #Using averages from comland to fill in categories
   herring[, MKTCAT := 5]
+
   herring[, TONCL1 := 2]
+
   herring[, UTILCD := 0]
 
   #compute price/utilization from CF tables
   herring.comland <- comland[NESPP3 == 168, ]
+
+
   #Price from comland
   herring.price <- herring.comland[, (sum(SPPVALUE) / sum(SPPLIVMT)), by = c('YEAR', 'MONTH')]
+
+
   data.table::setnames(herring.price, 'V1', 'price')
+
   herring <- merge(herring, herring.price, by = c('YEAR', 'MONTH'), all.x = T)
+
+
+
   #Use 1964 prices for < 1964
   herring[YEAR < 1964, price := mean(herring[YEAR == 1964, price])]
   #Calculate SPPVALUE from price
@@ -46,15 +67,24 @@ comland_herring <- function(channel,comland) {
   #Utilization from comland
   herring.util <- herring.comland[, sum(SPPLIVMT), by = c('YEAR', 'MONTH', 'UTILCD')]
   data.table::setnames(herring.util, 'V1', 'SPPLIVMT')
+
+
   herring.util[, SPPLIVMT.ALL := sum(SPPLIVMT), by = c('YEAR', 'MONTH')]
+
   herring.util[, Prop := SPPLIVMT/SPPLIVMT.ALL]
+
+
   data.table::setorder(herring.util, YEAR, MONTH, Prop)
+
+
   herring.util[, cum.prop := cumsum(Prop), by = c('YEAR', 'MONTH')]
 
   #Apply proportions to Maine data set
   #Not pulled all the time - current through 2017
   herring[, Total := sum(SPPLIVMT), by = c('YEAR', 'MONTH')]
+
   herring[, Prop := SPPLIVMT / Total]
+
   data.table::setorder(herring, YEAR, MONTH, Prop)
   herring[, cum.prop := cumsum(Prop), by = c('YEAR', 'MONTH')]
 
@@ -76,6 +106,7 @@ comland_herring <- function(channel,comland) {
   herring[, NESPP3 := 168]
 
   data.table::setcolorder(herring, names(comland))
+
   #remove herring from data pull and add in Maine numbers
   comland <- data.table::rbindlist(list(comland[NESPP3 != 168, ], herring))
 
