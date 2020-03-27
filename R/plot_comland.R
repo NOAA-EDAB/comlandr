6825789dd5f2bd305cd60a9c13ce91e4e99da2bf
andybeet <22455149+andybeet@users.noreply.github.com>
2020-03-26 21:45:04 -0400

    converted plot_comland from script to function - removed template functions from documentation

--- R/plot_comland.R
+++ R/plot_comland.R
@@ -1,10 +1,41 @@
-#plots of comlands data
-library(ggplot2); library(data.table)
+#'plots of comlands data
+#'
+#'@param data data frame. Output from \code{comland.r}
+#'
+#'@importFrom ggplot2 "aes" "vars" "margin"
+#'
+#'@export
 
-comland.gear <- comland[, .(landings = sum(SPPLIVMT)), by = c('YEAR', 'GEAR')]
+
+
+plot_comland <- function(data) {
+
+  comland.gear <- data[, .(landings = sum(SPPLIVMT)), by = c('YEAR', 'GEAR')]
+
+
+  g <- ggplot2::ggplot(comland.gear) +
+    ggplot2::geom_line(aes(x = YEAR,
+                           y = landings)) +
+    ggplot2::facet_wrap(vars(GEAR), scales = 'free') +
+    ggplot2::labs(y = 'Landings, metric tons', x = 'Year') +
+    theme_facet() +
+    ggplot2::theme(plot.margin = margin(r = 10, l = 10))
+
+  plot(g)
+}
+
+
+
+#' Theme template
+#'
+#'@importFrom ggplot2 "element_blank" "element_rect" "element_text"
+#'@noRd
+
+#library(ggplot2); library(data.table)
+
 
 theme_ts <- function(...){
-  theme(
+  ggplot2::theme(
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     panel.background = element_blank(),
@@ -14,8 +45,13 @@ theme_ts <- function(...){
   )
 }
 
+#' Theme Template
+#'
+#'#'@importFrom ggplot2 "element_blank" "element_rect" "element_text"
+#'@noRd
+
 theme_facet <- function(...){
-  theme(
+  ggplot2::theme(
     strip.background = element_blank(),
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
@@ -26,11 +62,4 @@ theme_facet <- function(...){
   )
 }
 
-ggplot(comland.gear) +
-  geom_line(aes(x = YEAR,
-                y = landings)) +
-  facet_wrap(vars(GEAR), scales = 'free') + 
-  labs(y = 'Landings, metric tons',
-       x = 'Year') +
-  theme_facet() +
-  theme(plot.margin = margin(r = 10, l = 10))
+
