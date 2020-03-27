83a17ad76a230c7da888e497f8f7614fd5ae35f3
Sean Lucey <Sean.Lucey@NOAA.gov>
2020-03-27 15:46:35 -0400

    Added the ability to specify a range and fix y axis

--- R/plot_comland.R
+++ R/plot_comland.R
@@ -38,6 +38,8 @@ theme_facet <- function(...){
 #'
 #'@param data data frame. Output from \code{comland.r}
 #'@param by The variable within \code{data} that you want to plot by
+#'@param range Range of years to plot.  Default value plots all years.
+#'@param free.y Allow the y-axis to rescale per "By" group.
 #'
 #'@importFrom ggplot2 "aes" "vars" "margin"
 #'@importFrom data.table "setnames"
@@ -45,22 +47,44 @@ theme_facet <- function(...){
 #'@export
 
 
-plot_comland <- function(data, by) {
+plot_comland <- function(data, by, range = NA, free.y = T) {
   
+  #Sum landings and assign "By" category
   comland.sum <- data[, .(landings = sum(SPPLIVMT)), by = c('YEAR', by, 'EPU')]
+  if(!is.na(range[1])) comland.sum <- comland.sum[YEAR %in% range, ]
   data.table::setnames(comland.sum, by, 'By')
   
+  #Remove scientific notation from axis
   format.axis <- function(x) x / 10e3
   
-  g <- ggplot2::ggplot(comland.sum) +
-    ggplot2::geom_line(aes(x = YEAR,
-                           y = landings)) +
-    ggplot2::facet_grid(By ~ EPU, scale = 'free_y') +
-    ggplot2::scale_y_continuous(labels = format.axis) +
-    ggplot2::labs(y = expression('Landings, metric tons 10'^3), x = 'Year') +
-    theme_facet() +
-    ggplot2::theme(plot.margin = margin(r = 10, l = 10))
+  #Set up plot
+  if(free.y == T){
+    g <- ggplot2::ggplot(comland.sum) +
+      ggplot2::geom_line(aes(x = YEAR,
+                             y = landings)) +
+      ggplot2::facet_grid(By ~ EPU, scale = 'free_y') +
+      ggplot2::scale_y_continuous(labels = format.axis) +
+      ggplot2::labs(y = expression('Landings, metric tons 10'^3), x = 'Year') +
+      theme_facet() +
+      ggplot2::theme(plot.margin = margin(r = 10, l = 10),
+                     panel.spacing.x = unit(15, "points"),
+                     panel.spacing.y = unit(10, "points"))
+  }
   
+  if(free.y == F){
+    g <- ggplot2::ggplot(comland.sum) +
+      ggplot2::geom_line(aes(x = YEAR,
+                             y = landings)) +
+      ggplot2::facet_grid(By ~ EPU) +
+      ggplot2::scale_y_continuous(labels = format.axis) +
+      ggplot2::labs(y = expression('Landings, metric tons 10'^3), x = 'Year') +
+      theme_facet() +
+      ggplot2::theme(plot.margin = margin(r = 10, l = 10),
+                     panel.spacing.x = unit(15, "points"),
+                     panel.spacing.y = unit(10, "points"))
+  }
+  
+  #plot figure
   plot(g)
 }
 
