89aadd030ab3345146bcae98badcc24cf932f1e6
andybeet <22455149+andybeet@users.noreply.github.com>
2020-04-30 13:29:46 -0400

    Document data sets GEARs and EPUs

---
+++ R/EPUs.R
@@ -0,0 +1,22 @@
+#' EPUs:  Properties of Ecological Production Units (EPUs)
+#'
+#' Lists the four EPUs (Georges Bank (GB), Gulf of Maine (GOM), Mid Atlantic Bight (MAB), Scotian Shelf (SS)) with the statistical areas that make up each region and their area (km^2)
+#'
+#' @format A list of lists
+#' \describe{
+#'     \item{GB}{Georges Bank EPU}
+#'     \item{GOM}{Gulf of Maine EPU}
+#'     \item{MAB}{Mid Atlantic Bight EPU}
+#'     \item{SS}{Scotian Shelf EPU}
+#'}
+#'
+#'@section List components:
+#'
+#'Each list item is a list itself containg the following elements:
+#'
+#'\describe{
+#'    \item{statAreas}{statistical areas that comprise the EPU}
+#'    \item{area}{area in km^2 of toal EPU}
+#'}
+#'
+"EPUs"
