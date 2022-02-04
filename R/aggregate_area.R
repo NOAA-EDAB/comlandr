19caf9393658cc6173bcb252bbb63363b5f1cfd0
Sean Lucey <Sean.Lucey@NOAA.gov>
2022-02-03 22:40:43 -0500

    + allow.cartesian so you can assign multiple regions by proportion

--- R/aggregate_area.R
+++ R/aggregate_area.R
@@ -24,7 +24,7 @@ aggregate_area <- function(comData, userAreas, areaDescription, propDescription,
   setnames(areas, c(areaDescription, propDescription), c('newarea', 'prop'))
   
   #Merge new area descriptions to landings
-  new.area <- merge(comData, areas, by = c('NESPP3', 'AREA'), all.x = T)
+  new.area <- merge(comData, areas, by = c('NESPP3', 'AREA'), all.x = T, allow.cartesian=TRUE)
   
   #If no proportion assume 100% in
   validAreas <- unique(areas[, newarea])
