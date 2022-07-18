96c38abd9d8b003a48e1468b0ade84d074d8850b
Sean Lucey <Sean.Lucey@NOAA.gov>
2022-07-18 15:56:29 -0400

    Allow for more than 1 Var to be run with one function call

--- R/assign_unknown.R
+++ R/assign_unknown.R
@@ -22,7 +22,7 @@
 
 assign_unknown <- function (comland, var, 
                             aggStrata = c('NESPP3', 'YEAR', 'HY', 'QY', 'MONTH',
-                                          'NEGEAR', 'TONCL1')) {
+                                          'NEGEAR', 'TONCL1', 'AREA')) {
   
   #Assign Quarter/Half Years
   comland[MONTH == 0,       QY := 0]
@@ -35,63 +35,72 @@ assign_unknown <- function (comland, var,
   comland[QY %in% 1:2, HY := 1]
   comland[QY %in% 3:4, HY := 2]
   
-  #Change names of strata
-  strata <- paste0('STR', 1:length(aggStrata))
-  data.table::setnames(comland, c(aggStrata, var), c(strata, 'VAR'))
-  
-  #Identify records with known and unknown areas
-  known   <- comland[!VAR %in% c(0, 999), ]
-  known   <- known[!is.na(VAR), ]
-  unknown <- comland[ VAR %in% c(0, 999) |  is.na(VAR), ]
-  
-  #set output to known records only
-  comland.out <- data.table::copy(known)
-  
-  if(nrow(unknown) > 0){
-    cat(paste('Total', var, 'unknown records', nrow(unknown), '\n'))
-    
-    for(i in length(strata):1){
-      #Identify columns that are not part of the stratification
-      ext.col <- names(comland)[which(!names(comland) %in% c(strata[1:i], 
-                                                             'VAR', 'SPPLIVMT'))]
-      
-      #Remove from known
-      known[, c(names(known)[which(names(known) %in% ext.col)]) := NULL]
-      
-      #Sum landings per stratification
-      known.sum <- known[, .(VARMT = sum(SPPLIVMT)), by = c(strata[1:i], 'VAR')]
-      
-      #Match records but keep unmatched records to carry forward
-      match <- merge(known.sum, unknown, by = strata[1:i], all.y = T, 
-                     allow.cartesian = T)
-      
-      #Remove unmatched for next round
-      unknown <- match[is.na(VAR.x), ]
-      unknown[, c('VAR.x', 'VARMT') := NULL]
-      data.table::setnames(unknown, 'VAR.y', 'VAR')
-      
-      #Remove unmatched records from merge
-      match <- match[!is.na(VAR.x), ]
-      
-      #Determine proportion of known catch per area
-      match[, totlivmt := sum(VARMT), by = c(strata[1:i])]
-      match[, prop := VARMT / totlivmt]
-      
-      #Proportion catch from unknown areas to known areas
-      match[, newlivmt := SPPLIVMT * prop]
-      
-      #Drop extra columns
-      match <- match[, c('VAR.y', 'VARMT', 'totlivmt', 'prop', 'SPPLIVMT') := NULL]
-      data.table::setnames(match, c('VAR.x', 'newlivmt'), c('VAR', 'SPPLIVMT'))
-      
-      #Append new entries to output
-      comland.out <- data.table::rbindlist(list(comland.out, match), use.names = T)
-      cat(paste('Unknown records remaining', nrow(unknown), '\n'))
+  for(ivar in 1:length(var)){
+    if(var[ivar] %in% aggStrata){
+      strata <- aggStrata[which(aggStrata != var[ivar])]  
+    } else {
+      strata <- aggStrata
     }
+    
+    #Change names of strata
+    strata.code <- paste0('STR', 1:length(strata))
+    data.table::setnames(comland, c(strata, var[ivar]), c(strata.code, 'VAR'))
+    
+    #Identify records with known and unknown variable
+    known   <- comland[!VAR %in% c(0, 999), ]
+    known   <- known[!is.na(VAR), ]
+    unknown <- comland[ VAR %in% c(0, 999) |  is.na(VAR), ]
+    
+    #set output to known records only
+    comland.out <- data.table::copy(known)
+    
+    if(nrow(unknown) > 0){
+      cat(paste('Total', var[ivar], 'unknown records', nrow(unknown), '\n'))
+      
+      for(i in length(strata.code):1){
+        #Identify columns that are not part of the stratification
+        ext.col <- names(comland)[which(!names(comland) %in% c(strata.code[1:i], 
+                                                               'VAR', 'SPPLIVMT'))]
+        
+        #Remove from known
+        known[, c(names(known)[which(names(known) %in% ext.col)]) := NULL]
+        
+        #Sum landings per stratification
+        known.sum <- known[, .(VARMT = sum(SPPLIVMT)), by = c(strata.code[1:i], 
+                                                              'VAR')]
+        
+        #Match records but keep unmatched records to carry forward
+        match <- merge(known.sum, unknown, by = strata.code[1:i], all.y = T, 
+                       allow.cartesian = T)
+        
+        #Remove unmatched for next round
+        unknown <- match[is.na(VAR.x), ]
+        unknown[, c('VAR.x', 'VARMT') := NULL]
+        data.table::setnames(unknown, 'VAR.y', 'VAR')
+        
+        #Remove unmatched records from merge
+        match <- match[!is.na(VAR.x), ]
+        
+        #Determine proportion of known catch per area
+        match[, totlivmt := sum(VARMT), by = c(strata.code[1:i])]
+        match[, prop := VARMT / totlivmt]
+        
+        #Proportion catch from unknown areas to known areas
+        match[, newlivmt := SPPLIVMT * prop]
+        
+        #Drop extra columns
+        match <- match[, c('VAR.y', 'VARMT', 'totlivmt', 'prop', 'SPPLIVMT') := NULL]
+        data.table::setnames(match, c('VAR.x', 'newlivmt'), c('VAR', 'SPPLIVMT'))
+        
+        #Append new entries to output
+        comland.out <- data.table::rbindlist(list(comland.out, match), use.names = T)
+        cat(paste('Unknown records remaining', nrow(unknown), '\n'))
+      }
+    }
+    #Revert names for subsequent runs and the output
+    data.table::setnames(comland, c(strata.code, 'VAR'), c(strata, var[ivar]))
+    data.table::setnames(comland.out, c(strata.code, 'VAR'), c(strata, var[ivar]))
   }
-  
-  #Revert names for output
-  data.table::setnames(comland.out, c(strata, 'VAR'), c(aggStrata, var))
   
   #Drop QY and HY
   comland.out[, c('QY', 'HY') := NULL]
