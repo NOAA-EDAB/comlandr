9a3b753c18440e346c09e626c126546449d12f28
Sean Lucey <Sean.Lucey@NOAA.gov>
2022-07-26 14:43:25 -0400

    Changed unknown variable default to correct capitalization

--- R/get_comland_data.R
+++ R/get_comland_data.R
@@ -39,7 +39,7 @@ get_comland_data <- function(channel, filterByYear = NA, filterByArea = NA, useL
                              aggArea = F, userAreas = comlandr::mskeyAreas,
                              areaDescription = 'EPU', propDescription = 'MeanProp',
                              aggGear = F, userGears = comlandr::mskeyGears,
-                             fleetDescription = 'Fleet', unkVar = 'Area',
+                             fleetDescription = 'Fleet', unkVar = 'AREA',
                              knStrata = c('NESPP3', 'YEAR', 'HY', 'QY', 'MONTH',
                                           'NEGEAR', 'TONCL1', 'AREA')) {
   
