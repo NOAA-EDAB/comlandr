815351677ed3e9a411695fa95e2216127302ab0c
Sean Lucey <Sean.Lucey@NOAA.gov>
2022-12-16 11:37:33 -0500

    new area table that includes all epus and no proportions

---
+++ data-raw/create_soeAreas.R
@@ -0,0 +1,17 @@
+#Area for all species
+library(data.table); library(usethis); library(here)
+
+load(here('data-raw', 'SOE_species_list.RData'))
+nespp3 <- unique(species[, NESPP3])
+
+GOM <- data.table::CJ(AREA = comlandr::EPUs$GOM$statAreas, NESPP3 = nespp3, 
+                      MeanProp = 1, EPU = 'GOM')
+GB  <- data.table::CJ(AREA = comlandr::EPUs$GB$statAreas, NESPP3 = nespp3, 
+                      MeanProp = 1, EPU = 'GB')
+MAB <- data.table::CJ(AREA = comlandr::EPUs$MAB$statAreas, NESPP3 = nespp3, 
+                      MeanProp = 1, EPU = 'MAB')
+SS  <- data.table::CJ(AREA = comlandr::EPUs$SS$statAreas, NESPP3 = nespp3, 
+                      MeanProp = 1, EPU = 'SS')
+
+soeAreas <- data.table::rbindlist(list(GOM, GB, MAB, SS))
+usethis::use_data(soeAreas)
