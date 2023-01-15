ab33fcd299fc3b56505a2d219e5d8d69ac6371dc
Sean Lucey <Sean.Lucey@NOAA.gov>
2023-01-15 11:53:25 -0500

    Added combinations...removed NESPP3 and Year from stratification

--- R/assign_unknown.R
+++ R/assign_unknown.R
@@ -22,8 +22,8 @@
 
 
 assign_unknown <- function (comData, unkVar, 
-                            knStrata = c('NESPP3', 'YEAR', 'HY', 'QY', 'MONTH',
-                                         'NEGEAR', 'TONCL1', 'AREA')) {
+                            knStrata = c('HY', 'QY', 'MONTH', 'NEGEAR', 'TONCL1',
+                                         'AREA')) {
   
   call <- c(comData$call, dbutils::capture_function_call())
   
@@ -34,6 +34,7 @@ assign_unknown <- function (comData, unkVar,
   sql <- comData$sql
   comdata <- data.table::copy(comData[[1]])
   
+  comdata <- copy(comData$comland[NESPP3 == 789,]) #Testing - Delete when done
   #Assign Quarter/Half Years
   comdata[MONTH == 0,       QY := 0]
   comdata[MONTH %in% 1:3,   QY := 1]
@@ -44,6 +45,10 @@ assign_unknown <- function (comData, unkVar,
   comdata[QY == 0,     HY := 0]
   comdata[QY %in% 1:2, HY := 1]
   comdata[QY %in% 3:4, HY := 2]
+  
+  #Assign size classes
+  comdata[TONCL1 %in% 1:3, SIZE := 1] #Small
+  comdata[TONCL1 > 3,      SIZE := 2] #Large
   
   for(ivar in 1:length(unkVar)){
     if(unkVar[ivar] %in% knStrata){
@@ -78,60 +83,82 @@ assign_unknown <- function (comData, unkVar,
     #set output to known records only
     comdata.out <- data.table::copy(known)
     
+    #Create combinations of strata variables to match
+    combos <- c()
+    for(inum in length(strata.code):1){
+      #combinations of strata codes
+      num.combos <- data.table::setDT(list(gtools::combinations(length(strata.code), 
+                                                                inum, strata.code)))
+      combos <- rbindlist(list(combos, num.combos), fill = T)
+    }
+    
+    
     if(nrow(unknown) > 0){
       cat(paste('Total', unkVar[ivar], 'unknown records', nrow(unknown), '\n'))
       
-      for(i in length(strata.code):1){
-        #Identify columns that are not part of the stratification
-        ext.col <- names(comdata)[which(!names(comdata) %in% c(strata.code[1:i], 
-                                                               'VAR', 'SPPLIVMT'))]
-        
-        #Remove from known
-        known[, c(names(known)[which(names(known) %in% ext.col)]) := NULL]
-        
-        #Need to only match with known variables
-        #Step 1: Complete cases
-        known.all <- known[complete.cases(known), ]
-        known.some <- known[!complete.cases(known), ]
-        
-        #Sum landings per stratification
-        known.sum <- known.all[, .(VARMT = sum(SPPLIVMT)), 
-                               by = c(strata.code[1:i], 'VAR')]
-        
-        #Match records but keep unmatched records to carry forward
-        match <- merge(known.sum, unknown, by = strata.code[1:i], all.y = T, 
-                       allow.cartesian = T)
-        
-        #Remove unmatched for next round
-        unknown <- match[is.na(VAR.x), ]
-        unknown[, c('VAR.x', 'VARMT') := NULL]
-        data.table::setnames(unknown, 'VAR.y', 'VAR')
-        
-        #Remove unmatched records from merge
-        match <- match[!is.na(VAR.x), ]
-        
-        #Determine proportion of known catch per area
-        match[, totlivmt := sum(VARMT), by = c(strata.code[1:i], 'ID')]
-        #Catch zeros that lead to NaN
-        match[totlivmt == 0, totlivmt := 1]
-        
-        match[, prop := VARMT / totlivmt]
-        
-        #Proportion catch from unknown areas to known areas
-        match[, newlivmt := SPPLIVMT * prop]
-        match[, newvalue := SPPVALUE * prop]
-        
-        #Drop extra columns
-        match <- match[, c('VAR.y', 'VARMT', 'totlivmt', 'prop', 'SPPLIVMT', 
-                           'SPPVALUE', 'ID') := NULL]
-        data.table::setnames(match, c('VAR.x', 'newlivmt', 'newvalue'), 
-                             c('VAR', 'SPPLIVMT', 'SPPVALUE'))
-        
-        #Append new entries to output
-        comdata.out <- data.table::rbindlist(list(comdata.out, match), use.names = T)
-        cat(paste('Unknown records remaining', nrow(unknown), '\n'))
+      
+      for(icom in 1:nrow(combos)){
+        if(nrow(unknown) > 0){
+          stra.combo <- combos[icom, ]
+          num.stra.var <- stra.combo[, which(!is.na(stra.combo))]
+          stra.combo <- unlist(stra.combo[, ..num.stra.var])
+          
+          #Identify columns that are not part of the stratification
+          ext.col <- names(comdata)[which(!names(comdata) 
+                                          %in% c('NESPP3', 'YEAR', stra.combo, 'VAR',
+                                                 'SPPLIVMT'))]
+            
+          #Remove extra columns from known
+          known.simple <- copy(known)
+          known.simple <- known.simple[, c(names(known)[which(names(known) %in%
+                                                                ext.col)]) := NULL]
+            
+          #Need to only match with known variables
+          known.all <- known.simple[complete.cases(known.simple), ]
+            
+          #Sum landings per stratification
+          known.sum <- known.all[, .(VARMT = sum(SPPLIVMT)), 
+                                 by = c('NESPP3', 'YEAR', stra.combo, 'VAR')]
+            
+          #Match records but keep unmatched records to carry forward
+          match <- merge(known.sum, unknown, by = c('NESPP3', 'YEAR', stra.combo),
+                         all.y = T, allow.cartesian = T)
+            
+          #Remove unmatched for next round
+          unknown <- match[is.na(VAR.x), ]
+          unknown[, c('VAR.x', 'VARMT') := NULL]
+          data.table::setnames(unknown, 'VAR.y', 'VAR')
+            
+          #Remove unmatched records from merge
+          match <- match[!is.na(VAR.x), ]
+            
+          #Determine proportion of known catch per variable
+          match[, totlivmt := sum(VARMT), by = c(stra.combo, 'ID')]
+          #Catch zeros that lead to NaN
+          match[totlivmt == 0, totlivmt := 1]
+            
+          match[, prop := VARMT / totlivmt]
+            
+          #Proportion catch from unknown variable to known variable
+          match[, newlivmt := SPPLIVMT * prop]
+          match[, newvalue := SPPVALUE * prop]
+            
+          #Drop extra columns
+          match <- match[, c('VAR.y', 'VARMT', 'totlivmt', 'prop', 'SPPLIVMT', 
+                             'SPPVALUE', 'ID') := NULL]
+          data.table::setnames(match, c('VAR.x', 'newlivmt', 'newvalue'), 
+                               c('VAR', 'SPPLIVMT', 'SPPVALUE'))
+            
+          #Append new entries to output
+          comdata.out <- data.table::rbindlist(list(comdata.out, match), 
+                                               use.names = T)
+          cat(paste('After using', length(stra.combo), 
+          'variables: Unknown records remaining', nrow(unknown), '\n'))
+          }
+        }
       }
-    }
+    
+
     #Append any remaining unknown records
     comdata.out <- data.table::rbindlist(list(comdata.out, unknown[, ID := NULL]), 
                                          use.names = T)
