d89de26635940f83eced5554049538a87bc36108
andybeet <22455149+andybeet@users.noreply.github.com>
2024-09-14 09:40:19 -0400

    Added documentation to exported data sets - mskeyAreas - mskeyGears - soeAreas

---
+++ R/deflate.R
@@ -0,0 +1,16 @@
+#' deflate:  Index of dollar inflation
+#'
+#' This needs to be pulled directly from gov website in future
+#' This data set should be temporary since it will need to be updated each year
+#' It isn't clear where from
+#'
+#' @format A Data frame
+#' \describe{
+#'     \item{YEAR}{Year of index}
+#'     \item{MONTH}{Month of index}
+#'     \item{PPI}{Price Index}
+#'}
+#'
+#'
+#'
+"deflate"
---
+++ R/mskeyAreas.R
@@ -0,0 +1,17 @@
+#' mskeyAreas:  Area Species, EPU designations
+#'
+#' Option to pass to \code{get_comland_data} if the user wishes define proportion Statistical Areas
+#' based on findings in mskeyrun project. Statistical Area that straddle EPUs have landings proportioned among
+#' neighboring EPUs
+#'
+#' @format A Data frame
+#' \describe{
+#'     \item{AREA}{Statistical Area}
+#'     \item{NESPP3}{Gulf of Maine EPU}
+#'     \item{MeanProp}{Proportion of Statistical area to attribute to EPU}
+#'     \item{EPU}{Ecological Production Unit}
+#'}
+#'
+#'
+#'
+"mskeyAreas"
