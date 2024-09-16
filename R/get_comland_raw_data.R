6cf77ede78cb5558dbf9d53424a6b0a206a4acb9
andybeet <22455149+andybeet@users.noreply.github.com>
2024-09-15 21:53:09 -0400

    edited function docs to include inherited parameter definitions - switched docs to use ROracle rather than odbc

--- R/get_comland_raw_data.R
+++ R/get_comland_raw_data.R
@@ -2,12 +2,7 @@
 #'
 #' Connects to cfdbs and pulls fields from WOLANDS, WODETS, CFDETS
 #'
-#'@param channel an Object inherited from \link[DBI]{DBIConnection-class}. This object is used to connect
-#' to communicate with the database engine. (see \code{\link[dbutils]{connect_to_database}})
-#'@param endyear Numeric Scalar. Final year of query.
-#'@param landed Character String. Use landed weight ("y" - meatwt) for scallops and clams instead of live weight ("n" - livewt).
-#'@param out.dir path to directory where final output will be saved
-#'
+#'@inheritParams get_comland_data
 #'
 #'@return Data frame (data.table) (n x 10)
 #'Each row of the data.table represents a species record for a given tow/trip
@@ -29,42 +24,42 @@
 #'
 #'@export
 
-get_comland_raw_data <- function(channel, filterByYear = NA, filterByArea = NA, 
+get_comland_raw_data <- function(channel, filterByYear = NA, filterByArea = NA,
                                  useLanded = T, removeParts = T){
-  
+
   #If not specifying a year default to 1964 - 2019
   if(is.na(filterByYear[1])) filterByYear <- 1964:2019
   filteryears <- sqltext(filterByYear)
-    
+
   message(paste0("Pulling landings data from ",
   filterByYear[1], " to ", filterByYear[length(filterByYear)],
   ". This could take a while (> 1 hour) ... "))
-  
+
   # #Generate vector of tables to loop through
    if(any(filterByYear < 1964)) stop("Landings data start in 1964")
-  # 
+  #
   # #CAMS tables are on NEFSC_USER while the rest are on sole
   # channels <- rep('sole', length(filterByYear))
   # channels[which(filterByYear > 2019)] <- 'nova'
-  # 
+  #
   # #Name tables
   # tables <- as.numeric(c(substr(filterByYear[which(filterByYear <= 1993)], 3, 4),
   #             filterByYear[which(filterByYear >  1993)]))
-  # tables[which(tables > 2019)] <- 
+  # tables[which(tables > 2019)] <-
   #   paste0('CAMS_GARFO.CAMS_CFDETS', tables[which(tables > 2019)], 'AA')
-  # tables[which(tables > 1993 & tables <= 2019)] <- 
+  # tables[which(tables > 1993 & tables <= 2019)] <-
   #   paste0('CFDETS', tables[which(tables > 1993 & tables <= 2019)], 'AA')
   # tables[which(tables > 63 & tables <= 81)] <- paste0('WOLANDS', tables[which(tables > 63 & tables <= 81)])
   # tables[which(tables > 81 & tables <= 93)] <- paste0('WODETS',  tables[which(tables > 81 & tables <= 93)])
-  # 
+  #
   # #output objects
   # comland <- c()
   # sql <- c()
-  # 
+  #
   # for(itab in 1:length(tables)){
   #   #Use correct connection
   #   if(channels[itab] == 'sole') channel <- channelSole else channel <- channelNova
-  #   
+  #
   #   #Data query
   #   #Need to add mesh data post 1981
   #   if(substr(tables[itab], 1, 3) == 'WOL'){
@@ -83,12 +78,12 @@ get_comland_raw_data <- function(channel, filterByYear = NA, filterByArea = NA,
   #     }
   #     if(filterByYear[itab] > 1993 & filterByYear[itab] <= 2019){
   #       trip.table <- paste0('CFDETT',  filterByYear[itab], 'AA')
-  #     } 
+  #     }
   #     if(filterByYear[itab] > 1981 & filterByYear[itab] <= 1993){
   #       trip.table <- paste0('WODETT',  substr(filterByYear[itab], 3, 4))
   #     }
-  #     landings.qry <- paste("select a.year, a.month, a.negear, a.toncl1, a.nespp3, 
-  #                          a.nespp4, a.area, a.spplivlb, a.spplndlb, a.sppvalue, 
+  #     landings.qry <- paste("select a.year, a.month, a.negear, a.toncl1, a.nespp3,
+  #                          a.nespp4, a.area, a.spplivlb, a.spplndlb, a.sppvalue,
   #                          a.utilcd, b.mesh
   #                          from", tables[itab], "a,", trip.table, "b
   #                          where a.link = b.link")
@@ -99,39 +94,39 @@ get_comland_raw_data <- function(channel, filterByYear = NA, filterByArea = NA,
   #     comland.yr <- data.table::as.data.table(DBI::dbGetQuery(channel, landings.qry))
   #   }
     #Use Stockeff table
-    
-    landings.qry <- paste("select year, month, negear, toncl2, nespp3, nespp4, area, 
-                    utilcd, mesh, market_code, sum(spplivlb) as spplivlb, 
+
+    landings.qry <- paste("select year, month, negear, toncl2, nespp3, nespp4, area,
+                    utilcd, mesh, market_code, sum(spplivlb) as spplivlb,
                     sum(spplndlb) as spplndlb, sum(sppvalue) as sppvalue
                     from stockeff.mv_cf_landings
                     where year in (", filteryears, ")
-                    group by year, month, negear, toncl2, nespp3, nespp4, area, 
+                    group by year, month, negear, toncl2, nespp3, nespp4, area,
                     utilcd, mesh, market_code")
     comland <- data.table::as.data.table(DBI::dbGetQuery(channel, landings.qry))
-  
+
     sql <- landings.qry
 
     #Identify small/large mesh fisheries
     comland[MESH <= 3, MESHCAT := 'SM']
     comland[MESH >  3, MESHCAT := 'LG']
     comland[, MESH := NULL]
-    
+
     # Use landed weight instead of live weight for shellfish
     if(useLanded) {comland[NESPP3 %in% 743:800, SPPLIVLB := SPPLNDLB]}
     comland[, SPPLNDLB := NULL]
-    
+
     # Remove fish parts so live weight is not double counted
     if(removeParts){
-      comland <- comland[!NESPP4 %in% c('0119', '0123', '0125', '0127', '0812', 
-                                        '0819', '0828', '0829', '1731', '2351', 
+      comland <- comland[!NESPP4 %in% c('0119', '0123', '0125', '0127', '0812',
+                                        '0819', '0828', '0829', '1731', '2351',
                                         '2690', '2699', '3472', paste0(348:359, 8),
-                                        '3868', paste0(469:471, 4), paste0(480:499, 8), 
+                                        '3868', paste0(469:471, 4), paste0(480:499, 8),
                                         '5018', '5039', '5261', '5265'), ]
     }
-    
+
     #Remove NESPP4
     comland[, NESPP4 := NULL]
-    
+
     # #Sum landings and value
     # data.table::setkey(comland.yr,
     #                    YEAR,
@@ -146,38 +141,38 @@ get_comland_raw_data <- function(channel, filterByYear = NA, filterByArea = NA,
     # comland.yr[, V1 := sum(SPPLIVLB, na.rm = T), by = key(comland.yr)]
     # #value
     # comland.yr[, V2 := sum(SPPVALUE, na.rm = T), by = key(comland.yr)]
-    
+
     # #Create market category
     # comland.yr[, MKTCAT := substr(NESPP4, 4, 4)]
-    # 
+    #
     # #Remove extra rows/columns
     # comland.yr <- unique(comland.yr, by = key(comland.yr))
     # comland.yr[, c('SPPLIVLB', 'SPPLNDLB', 'SPPVALUE', 'NESPP4') := NULL]
 
     # #Rename summed columns
     # data.table::setnames(comland.yr, c('V1', 'V2'), c('SPPLIVLB', 'SPPVALUE'))
-    # 
+    #
     # comland <- data.table::rbindlist(list(comland, comland.yr))
-    # 
+    #
     # message("Pulled data from ",tables[itab]," ...")
-    # 
+    #
     # }
 
   #Convert number fields from chr to num
   numberCols <- c('YEAR', 'MONTH', 'NEGEAR', 'TONCL2', 'NESPP3', 'UTILCD', 'AREA')
   comland[, (numberCols):= lapply(.SD, as.numeric), .SDcols = numberCols][]
-  
+
   #Adjust pounds to metric tons
   comland[, SPPLIVMT := SPPLIVLB * 0.00045359237]
   comland[, SPPLIVLB := NULL]
-  
+
   # #standardize YEAR field
   # comland[YEAR < 100, YEAR := YEAR + 1900L]
 
   #Add Nationality Flag
   comland[, US := T]
-  
-  return(list(comland = comland[], 
+
+  return(list(comland = comland[],
               sql     = sql))
 }
 
