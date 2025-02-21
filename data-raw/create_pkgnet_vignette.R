42934627d97ba3b51f0d2b35141667e7867d80ad
atyrell3 <77738923+atyrell3@users.noreply.github.com>
2025-02-21 17:35:18 -0500

    update pkgnet script

--- data-raw/create_pkgnet_vignette.R
+++ data-raw/create_pkgnet_vignette.R
@@ -1,9 +1,12 @@
 
-
-devtools::load_all()
+# install.packages("pkgnet")
 remotes::install_github("noaa-edab/comlandr", ref = "dev")
 
 result <- pkgnet::CreatePackageReport(pkg_name = "comlandr",
                                       pkg_path = ".")
 
 pkgnet::CreatePackageVignette()
+
+# this doesn't work
+knitr::knit(input = here::here("vignettes/pkgnet-report.Rmd"),
+            output = here::here("data-raw/pkgnet-report.html"))
