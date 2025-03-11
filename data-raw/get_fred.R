fb7451f0b57c7ec8e9640d8ba0dbf0be580119ae
andybeet <22455149+andybeet@users.noreply.github.com>
2025-03-11 14:32:30 -0400

    make get_fred a function

--- data-raw/get_fred.R
+++ data-raw/get_fred.R
@@ -1,10 +1,21 @@
-#' need to incoprporate this into the package to pull
-#' directly from th federal reserve
-FRED_key <-  Sys.getenv("API_KEY")
-fredr::fredr_set_key(FRED_key)
-deflators <- fredr::fredr(
-  series_id = "GDPDEF",
-  observation_start = as.Date("1981-01-01"),
-  frequency = "q")
-print(deflators)
+#' Pulls FRED data from federal reserve bank
+#'
+#' From Jan 1981. Data is quarterly
+
+get_fred <- functon() {
+  FRED_key <-  Sys.getenv("API_KEY")
+  fredr::fredr_set_key(FRED_key)
+  deflators <- fredr::fredr(
+    series_id = "GDPDEF",
+    observation_start = as.Date("1981-01-01"),
+    frequency = "q")
+
+  formatted <- deflators |>
+    dplyr::mutate(YEAR = lubridate::year(date),
+                  MONTH = lubridate::month(date)) |>
+    dplyr::select(YEAR,MONTH,value)
+
+  return(formatted)
+
+}
 
