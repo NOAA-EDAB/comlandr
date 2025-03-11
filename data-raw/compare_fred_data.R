2ee41f1c2d06e66c3c4932e583e15bcefe21ead6
andybeet <22455149+andybeet@users.noreply.github.com>
2025-03-11 16:41:49 -0400

    add comparison function of data

---
+++ data-raw/compare_fred_data.R
@@ -0,0 +1,45 @@
+#' Compare the new FRED dataset with the old
+#' determine wha changes
+
+compareData <- function(current,new) {
+
+
+  #######################################################
+  #######################################################
+  ## Checks on dimension
+  #######################################################
+  #######################################################
+
+  output <- list()
+  if (all.equal(current,new)) {
+    # No change
+    output$isEqual <- T
+  } else {
+    # Changes found
+    output$isEqual <- F
+    # check number of rows
+    output$rowNum <- c(nrow(new),nrow(current))
+    # remove new rows and compare
+    if(nrow(new) > nrow(current)) {
+      newnew <- new |>
+        head(-(nrow(new)-nrow(current)))
+      if(all.equal(newnew,current)) {
+        # just new rows added
+        output$newRows <- T
+      } else {
+        # create plots
+        # new |>
+        #   dplyr::left_join(current, by = c("YEAR","MONTH"))
+        #
+        # p <- ggplot2::ggplot(data=current)
+        #
+        # output$ggplot <- p
+
+      }
+    }
+  }
+
+
+  return(output)
+
+}
