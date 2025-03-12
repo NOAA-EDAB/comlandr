1c939316212c6a01625a792c1e92a62e2f5d7637
andybeet <22455149+andybeet@users.noreply.github.com>
2025-03-12 14:18:17 -0400

    add ggplot to compare data function

--- data-raw/compare_fred_data.R
+++ data-raw/compare_fred_data.R
@@ -1,7 +1,7 @@
 #' Compare the new FRED dataset with the old
 #' determine wha changes
 
-compare_fred_data <- function(current,new) {
+compare_fred_data <- function(old,new) {
 
 
   #######################################################
@@ -11,29 +11,41 @@ compare_fred_data <- function(current,new) {
   #######################################################
 
   output <- list()
-  if (all.equal(current,new)) {
+  if (isTRUE(all.equal(old,new))) {
     # No change
     output$isEqual <- T
   } else {
     # Changes found
     output$isEqual <- F
     # check number of rows
-    output$rowNum <- c(nrow(new),nrow(current))
+    output$rowNum$new <- nrow(new)
+    output$rowNum$old <- nrow(old)
     # remove new rows and compare
-    if(nrow(new) > nrow(current)) {
+    if(nrow(new) > nrow(old)) {
       newnew <- new |>
-        head(-(nrow(new)-nrow(current)))
-      if(all.equal(newnew,current)) {
+        head(-(nrow(new)-nrow(old)))
+      if(isTRUE(all.equal(newnew,old))) {
         # just new rows added
         output$newRows <- T
       } else {
+        output$newRows <- F
         # create plots
-        # new |>
-        #   dplyr::left_join(current, by = c("YEAR","MONTH"))
-        #
-        # p <- ggplot2::ggplot(data=current)
-        #
-        # output$ggplot <- p
+        d <- new |>
+          dplyr::left_join(old, by = c("YEAR","MONTH")) |>
+          dplyr::rename(old = value.y,
+                        new = value.x) |>
+          tidyr::pivot_longer(cols=-c(MONTH,YEAR),
+                              names_to = "source",
+                              values_to = "value") |>
+          dplyr::mutate(date = as.Date(paste0(YEAR,"-",MONTH, "-01", sep="")))
+
+        p <- ggplot2::ggplot(data = d) +
+          ggplot2::geom_line(ggplot2::aes(x=date,y = value,
+                                          color = source),na.rm =T) +
+          ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90,
+                                                            hjust = 1,
+                                                            vjust = 0.5))
+        output$ggplot <- p
 
       }
     }
