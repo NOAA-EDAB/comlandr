2356d2b35f1ffd0f35331c6bbdf5b71cc7c40925
Robert.Gamble <1387721408113302@mil>
2026-08-11 11:26:58 -0400

    Just testing that I can make a change that works.

--- R/get_species.R
+++ R/get_species.R
@@ -51,6 +51,7 @@
 #
 
 get_species <- function(channel, species = "all") {
+  print("Inside get species")
   # creates the sql based on user input
   sqlStatement <- dbutils::create_sql(
     species,
