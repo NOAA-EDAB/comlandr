848d7e8c8e511902264670e773863779064702d7
Robert Gamble <Robert.Gamble@noaa.gov>
2026-08-21 09:13:06 -0400

    Update R/get_comland_data.R
    This worked.  Thanks for the catch!
    
    Co-authored-by: Andy Beet <22455149+andybeet@users.noreply.github.com>

--- R/get_comland_data.R
+++ R/get_comland_data.R
@@ -161,7 +161,10 @@ get_comland_data <- function(
       useLanded,
       useHerringMaine
     )
-    comland.foreign <- dplyr::filter(comland.foreign, YEAR %in% filterByYear)
+    if (!is.na(filterByYear)) {
+      comland.foreign <- dplyr::filter(comland.foreign, YEAR %in% filterByYear)
+    }
+
 
     #Combine foreign landings
     comland$comland <- data.table::rbindlist(
